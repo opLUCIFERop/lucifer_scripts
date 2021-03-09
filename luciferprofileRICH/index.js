5a45e578c8d9a81cbb234a42dc77500a2bb14ea3
LUCIFER <manoharsharma1212@gmail.com>
2021-03-10 00:41:17 +0530

    inital commit

--- luciferprofileRICH/index.js
+++ luciferprofileRICH/index.js
@@ -9,7 +9,7 @@ assets : {
 large_image : "lucifer_logo",
 large_text : "Playing LUCIFER op" // THIS WILL SHOW AS "Playing <Status>" from the outisde
 },
-buttons : [{label : "JOIN AURP" , url : "https://discord.gg/yn2Us3Y4Zc"}]
+buttons : [{label : "JOIN LUCIFER CLUBHOUSE" , url : "https://discord.gg/yn2Us3Y4Zc"}]
 }
 })
 })
